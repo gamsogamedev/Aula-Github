f1a927058cbdbeb6e71e7eac1748529351211dda
Lucas Balieiro <lucas.macbali@gmail.com>
2026-05-16 11:27:50 -0300

    Removendo assets

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,14 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var animatedSprite: AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed('ui_left'):
-		animated_sprite_2d.flip_h = true
+		animatedSprite.flip_h = true
 	if Input.is_action_just_pressed('ui_right'):
-		animated_sprite_2d.flip_h = false
+		animatedSprite.flip_h = false
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -19,13 +19,13 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite_2d.animation = "Run"
+		#animatedSprite.animation = "Run"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite_2d.animation = "Idle"
+		#animatedSprite.animation = "Idle"
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animated_sprite_2d.animation = "Jump"
+		#animatedSprite.animation = "Jump"
 
 	move_and_slide()
